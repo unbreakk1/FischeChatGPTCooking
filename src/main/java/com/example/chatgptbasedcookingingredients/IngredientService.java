57db74217ee61cd0dc75d643857d796592cb31d8
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-02-26 16:10:57 +0100

    Initial commit

---
+++ src/main/java/com/example/chatgptbasedcookingingredients/IngredientService.java
@@ -0,0 +1,19 @@
+﻿package com.example.chatgptbasedcookingingredients;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class IngredientService
+{
+    private final RestClient restClient;
+
+    public IngredientService(@Value("${OPENAI_KEY}") String key,RestClient.Builder restClient)
+    {
+        this.restClient = restClient
+                .baseUrl("https://api.openai.com/v1/chat/completions")
+                .defaultHeader("Authorization", "Bearer " + key)
+                .build();
+    }
+}
